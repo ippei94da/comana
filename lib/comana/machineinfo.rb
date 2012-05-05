a616c1940cfb2743655ed85111d68e8533f6a31f
ippei94da <ippei94da@gmail.com>
2012-05-05 17:06:22 +0900

    small revision

--- lib/comana/machineinfo.rb
+++ lib/comana/machineinfo.rb
@@ -6,10 +6,27 @@ require "yaml"
 #
 #
 #
-class Machineinfo
+class MachineInfo
+
+  class NoEntryError < Exception; end
+
   #
-  def initialize(data_file)
-    @data = YAML.load_file(data_file)
+  def initialize(data)
+    @data = data
+  end
+
+  def self.load_file(data_file)
+    #pp data_file
+    #pp ENV["PWD"]
+    #pp File.open(DATA_FILE, "r").readlines
+    data = YAML.load_file(data_file)
+    MachineInfo.new data
+    #pp @data
+  end
+
+  def get_host(host)
+    raise NoEntryError unless @data.has_key?(host)
+    @data[host]
   end
 
 end
--- spec/machineinfo_spec.rb
+++ spec/machineinfo_spec.rb
@@ -1,31 +1,56 @@
 #! /usr/bin/env ruby
 # coding: utf-8
 
+require "fileutils"
+
 require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
 require "comana/machineinfo.rb"
 
-describe Machineinfo do
-  before { @k00 = Machineinfo.new("spec/machineinfo")}
-
-  describe "#initialize" do
+describe MachineInfo do
+  describe "#load_file" do
     context "not exist machineinfo file" do
-      it ？
-      lambda{ Machineinfo.new("") }.should raise_error(Errno::ENOENT)
+      DATA_FILE = "spec/machineinfo"
+      #DATA_FILE = "spec/dummy.yaml"
+      #data = {
+      #  "Host1" => { "data1" => "1-1", "data2" => "1-2" },
+      #  "Host2" => { "data1" => "2-1", "data2" => "2-2" },
+      #}
+      #io = File.open(DATA_FILE, "w")
+      #YAML.dump(data, io)
+      #io.close
+
+      #pp File.open(DATA_FILE, "r").readlines
+
+      it { lambda{MachineInfo.load_file(DATA_FILE)}.should_not raise_error}
+
+      mi00 = MachineInfo.load_file(DATA_FILE)
+      it {mi00.get_host("Host1").should == { "data1" => "1-1", "data2" => "1-2" } }
+
+      #FileUtils.rm DATA_FILE
     end
 
+    context "not exist machineinfo file" do
+      it { lambda{ MachineInfo.load_file("") }.should raise_error(Errno::ENOENT) }
+    end
   end
 
-  #describe "#initialize" do
-  #  subject { @k00.method }
+  describe "#get_host" do
+    before do
+      @mi00 = MachineInfo.new({
+        "Host1" => { "data1" => "1-1", "data2" => "1-2" },
+        "Host2" => { "data1" => "2-1", "data2" => "2-2" },
+      })
+    end
 
-  #  context "Case A" do
-  #    it { should be_nil }
-  #  end
+    context "exist in data" do
+      subject { @mi00.get_host("Host1") }
+      it {should == { "data1" => "1-1", "data2" => "1-2" } }
+    end
 
-  #  context "Case B" do
-  #    before {}
-  #    it { should eq "value2" }
-  #  end
-  #end
+    context "no entry" do
+      it {lambda{@mi00.get_host("")}.should raise_error(MachineInfo::NoEntryError)}
+    end
+  end
+
 end
 
